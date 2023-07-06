c10f08fbfc98e22ea7ba9aecf9104fd2f4b2282a
Jérémy Ghesquière <jeremy@MacBook-Air-de-Jeremy.local>
2023-07-06 10:55:01 +0200

    Debut du projet, avancement sur le header et le footer et également la création de tous les projets

---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul class="navbar">
+        <li class="navbar-li"><a class="navbar-a" href="#home">Accueil</a></li>
+        <li class="navbar-li"><a class="navbar-a" href="#news">Articles</a></li>
+        <li class="navbar-li" style="float:right"><a class="active navbar-a" href="#about"><i class="fa-solid fa-user-plus"></i></a></li>
+        <li class="navbar-li" style="float:right"><a class="navbar-a" href="#contact">Contact</a></li>
+    </ul>
+    <img class="img_header" src="./src/image/f1headercut.png" alt="Monoplace F1 de Max Verstappen sous la pluie">
+</nav>
